a4cfc9f010e724b75332d0302f76d8e7cc66c7ea
EvgenyVolenko <evolenko@inbox.ru>
2023-04-05 17:20:07 +0300

    HW 1/98

--- Seminar_03/HW_03/Main.java
+++ Seminar_03/HW_03/Main.java
@@ -2,6 +2,24 @@ package Seminar_03.HW_03;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("123");
+        
+    VectorList<Integer> vectorList = new VectorList<>();
+    vectorList.add(1);
+    vectorList.add(4);
+    vectorList.add(6);
+     vectorList.add(7);
+     vectorList.add(15);
+
+     for (Integer item : vectorList) {
+        System.out.println(item);
+     }
+
+     VectorList<String> vectorString = new VectorList<>();
+     vectorString.add("Первый");
+     vectorString.add("Второй");
+
+     for (String str : vectorString) {
+        System.out.println(str);
+     }
     }
 }
