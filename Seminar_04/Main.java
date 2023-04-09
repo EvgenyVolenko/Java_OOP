19c42a4ed42e1953921fe8c09c6f3f7e7b6f6411
Evgeny <evolenko@inbox.ru>
2023-04-10 00:29:59 +0300

    Added HW 4/5

--- Seminar_04/Main.java
+++ Seminar_04/Main.java
@@ -4,10 +4,10 @@ public class Main {
         Team<Thief> thiefTeam = new Team<>();
 
         Archer robin = new Archer("Робин", 50, new Bow(20), new HardShield(10));
-        Archer gud = new Archer("Гуд", 70, new Bow(15), new HardShield(0));
+        Archer gud = new Archer("Гуд", 70, new Bow(15), new StandartShield(10));
         Archer sniper = new Archer("Снайпер", 80, new Bow(25), new HardShield(25));
 
-        Thief djek = new Thief("Джек", 50, new Knife(), new StandartShield(5));
+        Thief djek = new Thief("Джек", 100, new Knife(), new StandartShield(5));
         Thief potr = new Thief("Потрошитель", 30, new Knife(), new StandartShield(0));
         Thief tester = new Thief("Тестер", 55, new Knife(), new StandartShield(10));
 
@@ -23,7 +23,7 @@ public class Main {
         System.out.println(thiefTeam);
         System.out.println();
     
-        Battle fight = new Battle(robin, potr);
+        Battle fight = new Battle(gud, djek);
         fight.run();
     }
 }
